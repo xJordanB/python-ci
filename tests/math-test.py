8eb749a72033d4bc8a8ecff71984b1b07ef5913c
xJordanB <brookmanjordan.96@gmail.com>
2020-03-07 12:55:14 +0000

    New Test

--- tests/math-test.py
+++ tests/math-test.py
@@ -6,3 +6,8 @@ class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make tests fail
         self.assertEqual(Math.addition(3, 4), 8)
+
+    def test_addition_duplicate(self):
+        self.assertEqual(Math.addition(4, 5), 10)
+
+
